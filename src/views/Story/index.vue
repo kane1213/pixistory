35d9536bbba0eda6c74c3ed5f869d0c78a1de9e6
kane1213 <kane711213@gmail.com>
2021-04-07 22:18:12 +0800

    refactor

--- src/views/Story/index.vue
+++ src/views/Story/index.vue
@@ -13,9 +13,8 @@ import { TweenMax, TimelineMax } from 'gsap'
 const story = ref(null)
 const bunny = ref(null)
 
-const material: string[] = ['meats.png', 'ingredient1.jpg', 'ingredient2.jpg']
-
-const imgs = [ ...material.map(str => `/src/assets/images/recipe1/${str}`), ...new Array(38).fill('').map((v,i)=>`/src/assets/images/circles/c${i+1}.png`)]
+const material: string[] = ['meats.png', 'ingredient1.png', 'ingredient2.png', ...new Array(6).fill(1).map((v, i) => `step${v + i}.png`)]
+const imgs = material.map(str => `/src/assets/images/recipe1/${str}`)
 var phytouch = null
 const { clientWidth, clientHeight } = document.getElementById('mainLayout')
 const app = new PIXI.Application({
@@ -38,79 +37,56 @@ onMounted(() => {
   app.loader
     .add(imgs)
     .load((loader, resources) => {
-      const _meats = new PIXI.Sprite.from(imgs[0])
+
+      const _sprites: PIXI.Sprite[] = imgs.map((path):PIXI.Sprite => new PIXI.Sprite.from(path))
       const _meatText = new PIXI.Text('ローストビーフ',{fontSize: 32, fill : 0xff1010, align : 'center'})
-      const _ingre1 = new PIXI.Sprite.from(imgs[1])
-      const _ingre2 = new PIXI.Sprite.from(imgs[2])
-
-
-      const _scaleRate = app.renderer.width / _meats.width
-      _meats.x = app.renderer.width * .5
-      _meats.y = 0
-      _meats.anchor.x = .5
-      _meats.anchor.y = .5
-      _meats.alpha = 0
-      _meats.scale.x = _meats.scale.y = _scaleRate
-      
+      const { width: appWidth, height: appHeight } = app.renderer
+      _sprites.forEach((sprite,idx) => {
+        senceOne.addChild(sprite)
+        if (idx > 0) {
+          sprite.alpha = 0
+          sprite.y = 0
+        } else {
+          sprite.y = appHeight * .5
+        }
+        if (sprite.width > appWidth) sprite.scale.x = sprite.scale.y = appWidth / sprite.width
+        sprite.anchor.x = sprite.anchor.y = .5
+        sprite.x = appWidth * .5
+        
+      })
 
       _meatText.x = app.renderer.width - 15
       _meatText.y = app.renderer.height * .5
       _meatText.alpha = 0
-      _meatText.anchor.x = 1
-
-      const _scaleRate1 = app.renderer.width / _ingre1.width
-      const _scaleRate2 = app.renderer.width / _ingre2.width
-
-      _ingre1.x = _ingre2.x = app.renderer.width * .5
-      _ingre1.y = _ingre2.y = app.renderer.height * .5
-      _ingre1.anchor.x = _ingre1.anchor.y = _ingre2.anchor.x = _ingre2.anchor.y = .5
-      _ingre1.alpha = _ingre2.alpha = 0
-
-      _ingre1.scale.x = _ingre1.scale.y = _scaleRate1
-      _ingre2.scale.x = _ingre2.scale.y = _scaleRate2
-
-      
-
-      senceOne.addChild(_meats)
+      _meatText.anchor.x = 1      
       senceOne.addChild(_meatText)
-      senceOne.addChild(_ingre1)
-      senceOne.addChild(_ingre2)
-      // const _bunny = new PIXI.Sprite.from(imgs[1])  //
-      // _bunny.x = 0 // app.renderer.width / 2
-      // _bunny.y = app.renderer.height / 2
-      // _bunny.anchor.x = 0.5
-      // _bunny.anchor.y = 0.5
-      // _bunny.alpha = 0
       // senceOne.addChild(_bunny)
       // app.ticker.add(() => {
       //   _bunny.rotation += 0.01
       // })
-
+      let _delay = 0
+      const stepDelay = 1 / ((_sprites.length - 1) * 2 + 1 )
       const allTimeLine = new TimelineMax({ paused: true })
-      const timeline1 = new TimelineMax({ delay: 0.0 })
-      const timeline2 = new TimelineMax({ delay: .1 })
-      const timeline3 = new TimelineMax({ delay: .2 })
-      const timeline4 = new TimelineMax({ delay: .3 })
-      const timeline5 = new TimelineMax({ delay: .4 })
-      const timeline6 = new TimelineMax({ delay: .5 })
-      const timeline7 = new TimelineMax({ delay: .6 })
-     
-      timeline1.add(new TweenMax(_meats, 0.05, { alpha: 1, y: app.renderer.height * .5 }), 0)
-      timeline2.add(new TweenMax(_meatText, 0.05, { alpha: 1, y: app.renderer.height * .75 }), 0)
-      timeline3.add(new TweenMax([_meats, _meatText], 0.1, { alpha: 0 }), 0)
-      timeline4.add(new TweenMax(_ingre1, 0.05, { alpha: 1 }), 0)
-      timeline5.add(new TweenMax(_ingre1, 0.05, { y: 0, alpha: 0 }), 0)
-      timeline6.add(new TweenMax(_ingre2, 0.05, { alpha: 1 }), 0)
-      timeline7.add(new TweenMax(_ingre2, 0.05, { y: 0, alpha: 0 }), 0)
+      const timelines = new Array(2).fill('').map((v, i) => new TimelineMax({delay: (_delay++) * stepDelay}))
 
-      allTimeLine.add(timeline1, 0)
-      allTimeLine.add(timeline2, 0)
-      allTimeLine.add(timeline3, 0)
-      allTimeLine.add(timeline4, 0)
-      allTimeLine.add(timeline5, 0)
-      allTimeLine.add(timeline6, 0)
-      allTimeLine.add(timeline7, 0)
-      
+      // timelines[0].add(new TweenMax(_sprites[0], stepDelay, { alpha: 1, y: app.renderer.height * .5 }), 0)
+      timelines[0].add(new TweenMax(_meatText, stepDelay, { alpha: 1, y: appHeight * .75 }), 0)
+      timelines[1].add(new TweenMax([_sprites[0], _meatText], stepDelay, { alpha: 0, y: appHeight }), 0)
+      _sprites.slice(1, _sprites.length).forEach((sprite,sidx) => {
+        const _timeline1 = new TimelineMax({delay: (_delay++) * stepDelay})
+        timelines.push(_timeline1)
+        _timeline1.add(new TweenMax(sprite, stepDelay * .6, { y: appHeight * .5, alpha: 1 }), 0)
+
+        if (sidx < _sprites.length - 2) {
+          const _timeline2 = new TimelineMax({delay: (_delay++) * stepDelay})
+          timelines.push(_timeline2)
+          _timeline2.add(new TweenMax(sprite, stepDelay, { y: appHeight, alpha: 0 }), 0)
+        }
+      })
+
+      timelines.forEach(_time => {
+        allTimeLine.add(_time, 0)
+      })
       
       phytouch = new PhyTouch({
         touch: '#story',
