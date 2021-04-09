1175c714c02dd26d58ae7819741caab78a8f0be8
kane1213 <kane711213@gmail.com>
2021-04-09 22:16:49 +0800

    change footer color

--- src/components/layout/footer.vue
+++ src/components/layout/footer.vue
@@ -1,3 +1,3 @@
 <template lang="pug">
-.footer.bg-gray-900.text-white.py-1.px-2.text-center.text-xs dk & sakura 2021
+.footer.bg-blue-300.text-white.py-1.px-2.text-center.text-xs dk & sakura 2021
 </template>
