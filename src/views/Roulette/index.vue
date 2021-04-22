7ea0e7ec051ec2d8101db6c915e6cad26d553451
Kane <kane@datacom.com.tw>
2021-04-22 16:51:39 +0800

    refactor

--- src/views/Roulette/index.vue
+++ src/views/Roulette/index.vue
@@ -1,18 +1,26 @@
 <template lang="pug">
-#roulette(ref="roulette")
+#roulette(ref="rouletteDom")
 </template>
 <script lang="ts">
 import { computed, defineComponent, ref, reactive, onMounted } from 'vue'
 import * as PIXI from 'pixi.js'
 import { TweenMax, TimelineMax, Linear, Strong } from 'gsap'
-
+interface RoulettePrize {
+  label: string,
+  occupy: number
+}
 export default defineComponent({
   setup () {
-    const roulette = ref()
+    const rouletteDom = ref()
     const generateColors = (num: Number): number[] => new Array(num).fill(0).map(() => Math.floor(Math.random()*16777215))
-
+    const rouletteSecs: RoulettePrize[] = [
+      { label: 'prizeOne', occupy: 1 },
+      { label: 'prizeTwo', occupy: 4 },
+      { label: 'prizeThree', occupy: 1 },
+      { label: 'prizeFour', occupy: 2 },
+    ]
     onMounted(() => {
-
+      // const containers: PIXI.Container[] = []
       const mayLayout: HTMLElement = document.getElementById('mainLayout')!
       const { clientWidth, clientHeight } = mayLayout
       const app = new PIXI.Application({
@@ -20,30 +28,52 @@ export default defineComponent({
         height: clientHeight,
         transparent: true
       });
-      const senceOne = new PIXI.Container()
-      app.stage.addChild(senceOne)
-      const num = 4
+      const roulette: PIXI.Container = new PIXI.Container()
+      app.stage.addChild(roulette)
+      const num = rouletteSecs.reduce((acc, sec):number => acc + sec.occupy ,0)
       
-      const graphics = new PIXI.Graphics();
       const arcAngle = Math.PI / (num / 2)
       const startAngle = arcAngle * 1
-      const outsideRadius = clientWidth * .5
-      const insideRadius = 50
+      const outsideRadius = 30 // clientWidth * .5
+      const insideRadius = 2
       const position = 0
       const colors = generateColors(num)
-      for (let i = 0; i < num; i++) {
-        // let angle = startAngle + i * arcAngle
-        let angle = Math.PI / 180 * (-90 - (360 / num / 2))
-        graphics.beginFill(colors[i])
-        graphics.arc(position, position, outsideRadius, angle, angle + arcAngle, false)
-        graphics.arc(position, position, insideRadius, angle + arcAngle, angle, true)
-        graphics.endFill()
-      }
-      senceOne.addChild(graphics)
-      senceOne.x = clientWidth * .5
-      senceOne.y = clientHeight * .5
-      if (roulette.value) {
-        const element: HTMLDivElement = roulette.value!
+
+      rouletteSecs.forEach((sec, idx) => {
+        const _container: PIXI.Container = new PIXI.Container()
+        const _graphics: PIXI.Graphics = new PIXI.Graphics()
+        const _previous: number = idx > 0
+          ? rouletteSecs[idx - 1].occupy
+          : 0
+        _graphics.beginFill(colors[idx])
+        _graphics.arc(position, position, outsideRadius, 0, arcAngle * sec.occupy, false)
+        _graphics.arc(position, position, insideRadius, arcAngle * sec.occupy, 0, true)
+        _graphics.endFill()
+        _container.addChild(_graphics)
+        _container.rotation = Math.PI / 180 * (360 / num * sec.occupy)
+        roulette.addChild(_container)
+      })
+
+      // for (let i = 0; i < num; i++) {
+      //   const _container = new PIXI.Container()
+      //   // containers.push(_container)
+      //   const _graphics = new PIXI.Graphics();
+      //   // let angle = startAngle + i * arcAngle
+      //   let angle = Math.PI / 180 * (-90 - (360 / num / 2))
+      //   _graphics.beginFill(colors[i])
+      //   _graphics.arc(position, position, outsideRadius, angle, angle + arcAngle, false)
+      //   _graphics.arc(position, position, insideRadius, angle + arcAngle, angle, true)
+      //   _graphics.endFill()
+      //   _container.addChild(_graphics)
+      //   _container.rotation = Math.PI / 180 * (360 / num * i)
+        
+      //   roulette.addChild(_container)
+      // }
+      
+      roulette.x = clientWidth * .5
+      roulette.y = clientHeight * .5
+      if (rouletteDom.value) {
+        const element: HTMLDivElement = rouletteDom.value!
         element.appendChild(app.view)
       }
 
@@ -56,14 +86,14 @@ export default defineComponent({
       // allTimeLine.add(lottery)
       // allTimeLine.play(1)
 
-      senceOne.interactive = senceOne.buttonMode = true
-      senceOne.addListener('click', function () {
+      roulette.interactive = roulette.buttonMode = true
+      roulette.addListener('click', function () {
         if (!allTimeLine.isActive()) {
           // allTimeLine.pause()
           // allTimeLine.remove(loop)
           // allTimeLine._repeat = 0
           // allTimeLine.resume()
-          allTimeLine.add(new TweenMax(senceOne, 5, { rotation: Math.PI / 180 * 360, ease: Linear.easeIn }))
+          allTimeLine.add(new TweenMax(roulette, 5, { rotation: Math.PI / 180 * 360, ease: Linear.easeIn }))
           allTimeLine.play()
         } else {
           
@@ -72,7 +102,7 @@ export default defineComponent({
 
     })
 
-    return { roulette }
+    return { rouletteDom }
   }
 })
 </script>
