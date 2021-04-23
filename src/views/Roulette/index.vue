7d743edb7b753894cde9cecbb2e981a50479411d
kane1213 <kane711213@gmail.com>
2021-04-23 22:10:35 +0800

    refactor

--- src/views/Roulette/index.vue
+++ src/views/Roulette/index.vue
@@ -35,21 +35,21 @@ export default defineComponent({
       const roulette: PIXI.Container = new PIXI.Container()
       app.stage.addChild(roulette)
       const num = rouletteSecs.reduce((acc, sec):number => acc + sec.occupy ,0)
-      
+      const average: number = 360 / num
       const arcAngle = Math.PI / (num / 2)
       // const startAngle = arcAngle * 1
-      const outsideRadius = 90 // clientWidth * .5
+      const outsideRadius = clientWidth * .5
       const insideRadius = 10
       const position = 0
       const colors = generateColors(num)
       let accumNum = 0
-      let accumAngle = 0
+      
       rouletteSecs.forEach((sec, idx) => {
-        // if (idx > 1) return
+        // if (idx > 2) return
         const _container: PIXI.Container = new PIXI.Container()
         const _graphics: PIXI.Graphics = new PIXI.Graphics()
         const _text: PIXI.Text = new PIXI.Text(`${idx + 1}`)
-
+        
         const _previewOccupy =  idx > 0  ? rouletteSecs[idx - 1].occupy : 0
         accumNum += _previewOccupy
 
@@ -64,8 +64,9 @@ export default defineComponent({
         _text.y = -120
         _container.addChild(_graphics)
         _container.addChild(_text)
-        const _defaultAngle = idx > 0 ? rouletteSecs[0].occupy * .5 : 0
-        _container.rotation = Math.PI / 180 * (360 / num * (idx > 0 ? _defaultAngle + sec.occupy * .5 : 0))
+        
+        
+        _container.rotation = Math.PI / 180 * (average * (idx > 0 ? sec.accum - (rouletteSecs[0].occupy * .5) + sec.occupy * .5 : 0))
         roulette.addChild(_container)
       })
 
@@ -76,41 +77,32 @@ export default defineComponent({
         element.appendChild(app.view)
       }
 
-      // const loop = new TweenMax(senceOne, 5, { rotation: arcAngle * (num), ease: Linear.easeNone })
+      const loop = new TweenMax(roulette, 5, { rotation: arcAngle * (num), ease: Linear.easeNone })
 
       // const lottery = new TweenMax(senceOne, 5, { rotation: Math.PI / 180 * 360, ease: Linear.easeIn })
 
-       const allTimeLine = new TimelineMax()  // { repeat: -1 }
-      // allTimeLine.add(loop)
+       const allTimeLine = new TimelineMax({ repeat: -1 })
+      allTimeLine.add(loop)
       // allTimeLine.add(lottery)
-      // allTimeLine.play(1)
+      allTimeLine.play()
       roulette.interactive = roulette.buttonMode = true
-      roulette.addListener('click', function () {
-        if (!allTimeLine.isActive()) {
-          // allTimeLine.pause()
-          // allTimeLine.remove(loop)
-          // allTimeLine._repeat = 0
-          // allTimeLine.resume()
-          // const prizeSec: RoulettePrize = rouletteSecs[Math.floor(Math.random() * rouletteSecs.length)]
-          // roulette.rotation = roulette.rotation % 360
-          const _children = allTimeLine.getChildren()
-          _children.forEach(child => {
-            allTimeLine.remove(child)
-          })
-
-          allTimeLine.clear()
-          const prize: number = Math.floor(Math.random() * rouletteSecs.length)
-          const { occupy, accum } = rouletteSecs[prize]
-          const half = 360 / num * occupy * (Math.random() * .95)
-          const accums = 360 / num * accum
-
-          const lottery = new TweenMax(roulette, 1, { rotation: Math.PI / 180 * (360 * 3 -90 - half - accums ), ease: Linear.easeIn })
-          allTimeLine.add(lottery)
-          roulette.rotation = 0
-          allTimeLine.play()
-        } else {
-          
-        }
+      roulette.addListener('click', () => {
+        // if (allTimeLine.isActive()) {
+        const _children = allTimeLine.getChildren()
+        _children.forEach(child => {
+          allTimeLine.remove(child)
+        })
+        allTimeLine.clear()
+        allTimeLine._repeat = 0
+        const prize: number = Math.floor(Math.random() * rouletteSecs.length)
+        const { occupy, accum } = rouletteSecs[prize]
+        const _startAngle = average * rouletteSecs[0].occupy * .5
+        const _accumAngle = average * accum
+        const _half = average * (occupy * (.1 + Math.random() * .9))
+        const lottery = new TweenMax(roulette, 1, { rotation: Math.PI / 180 * (360 * 3 - _accumAngle - _half + _startAngle), ease: Linear.easeIn })
+        allTimeLine.add(lottery)
+        roulette.rotation = 0
+        allTimeLine.play()
       })
 
     })
