c8b4c47396e7db35285a2589da2d7a6b21c9ec31
kane1213 <kane711213@gmail.com>
2021-06-19 22:03:11 +0800

    refator

---
+++ src/views/subtitle.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+.p-2 TEST
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import subtitle from '@/assets/test/subtitle.srt'
+export default defineComponent({
+  setup () {
+
+    
+    return { test: 'Testing'}
+  }
+})
+</script>
+
+<style lang="sass" scoped>
+
+</style>
