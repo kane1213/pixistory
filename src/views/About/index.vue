79951dc4783a408bb44fa81dd0dee81c28431bb2
kane1213 <kane711213@gmail.com>
2021-12-30 00:06:09 +0800

    websocket

--- src/views/About/index.vue
+++ src/views/About/index.vue
@@ -7,6 +7,25 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 export default defineComponent({
+  setup () {
 
+    const mySocket = new WebSocket("ws://localhost:3002");
+    // mySocket.onmessage = function(e) {
+    //   console.log(e.data);
+    //   alert('gogo')
+    // }
+    // mySocket.onmessage(() => {
+    //   alert('cc')
+    // })
+    mySocket.addEventListener('open', function (event) {
+      console.log('Open connection')
+      mySocket.send(JSON.stringify({ data: 'Hello Server!' }));
+    });
+    mySocket.addEventListener('message', function (event) {
+      console.log(`Message from server: ${event.data}`);
+  });
+
+    return {}
+  }
 })
 </script>
